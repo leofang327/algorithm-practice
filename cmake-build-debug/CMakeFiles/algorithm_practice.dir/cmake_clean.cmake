404d96765804c7342248e473ceabaebc7acfd7f7
leofang327 <leofang666@163.com>
2021-05-14 00:18:25 +0800

    add sort algorithm

---
+++ cmake-build-debug/CMakeFiles/algorithm_practice.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm_practice.dir/main.cpp.o"
+  "algorithm_practice"
+  "algorithm_practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithm_practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
